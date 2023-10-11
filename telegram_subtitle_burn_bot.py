ad0449de5b39bc4289b6a2bfa39594f2c1418b89
est-anime <134935100+est-anime@users.noreply.github.com>
2023-10-11 22:07:34 +0530

    Update telegram_subtitle_burn_bot.py

--- telegram_subtitle_burn_bot.py
+++ telegram_subtitle_burn_bot.py
@@ -11,7 +11,7 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
-    bot.send_message(message.chat.id, "Welcome to Video Encoder! Send video to burn subtitles.")
+    bot.send_message(message.chat.id, "Welcome to Video Encoder! Send a video to burn subtitles.")
 
 def download_file(file_path):
     file_info = bot.get_file(file_path)
@@ -28,7 +28,6 @@ def download_file(file_path):
         temp_file.write(response.content)
         return temp_file.name
 
-
 @bot.message_handler(content_types=['video'])
 def handle_video(message):
     bot.send_message(message.chat.id, "Received a video. Now please send the subtitle file (in .srt or .ass format) for burning into the video.")
@@ -38,8 +37,7 @@ def handle_video(message):
 def process_subtitle(message):
     if message.content_type == 'document':
         file_id = message.document.file_id
-        file_info = bot.get_file(file_id)
-        file_path = file_info.file_path
+        file_path = download_file(file_id)
         subtitle_extension = message.document.file_name.split('.')[-1].lower()
         if subtitle_extension not in ['srt', 'ass']:
             bot.send_message(message.chat.id, "Invalid subtitle file format. Please send a .srt or .ass file.")
@@ -47,19 +45,12 @@ def process_subtitle(message):
             bot.send_message(message.chat.id, "Processing... This may take some time.")
             start_time = time.time()
             video_path = download_file(file_path)
-            subtitle_path = download_file(file_path)
+            subtitle_path = download_file(file_id)
             video_with_subtitles = burn_subtitles(video_path, subtitle_path)
             end_time = time.time()
             processing_time = end_time - start_time
             bot.send_message(message.chat.id, f"Processing time: {processing_time:.2f} seconds")
             bot.send_video(message.chat.id, open(video_with_subtitles, 'rb'))
-
-def download_file(file_path):
-    file_info = bot.get_file(file_path)
-    response = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(TOKEN, file_path))
-    with NamedTemporaryFile(delete=False) as temp_file:
-        temp_file.write(response.content)
-        return temp_file.name
 
 def burn_subtitles(video_path, subtitle_path):
     video = VideoFileClip(video_path)
