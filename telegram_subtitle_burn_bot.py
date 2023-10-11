269206e0e470fee7afcce859e031f51640973309
est-anime <134935100+est-anime@users.noreply.github.com>
2023-10-11 21:45:30 +0530

    Update telegram_subtitle_burn_bot.py

--- telegram_subtitle_burn_bot.py
+++ telegram_subtitle_burn_bot.py
@@ -2,28 +2,39 @@ import os
 import telebot
 from moviepy.editor import VideoFileClip
 from moviepy.video.tools.subtitles import SubtitlesClip
+import time
 
 TOKEN = '6422706778:AAEkiNY4Qo65d3tZWfRYYlkeHVaLA3FnleU'
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def handle_start(message):
-    # Respond to the /start command with a welcome message
     bot.send_message(message.chat.id, "Welcome to my Telegram bot! Please type /help to see the available commands.")
 
 @bot.message_handler(content_types=['video'])
 def handle_video(message):
-    file_id = message.video.file_id
-    file_info = bot.get_file(file_id)
-    file_path = file_info.file_path
-    video_path = download_video(file_path)
-    subtitle_path = 'your_subtitles.srt'
-    
-    # Burn subtitles
-    video_with_subtitles = burn_subtitles(video_path, subtitle_path)
-    
-    # Send the video with subtitles back to the user
-    bot.send_video(message.chat.id, open(video_with_subtitles, 'rb'))
+    bot.send_message(message.chat.id, "Received a video. Now please send the subtitle file (in .srt or .ass format) for burning into the video.")
+    # Store the received video message and user chat ID for later use
+    bot.register_next_step_handler(message, process_subtitle)
+
+def process_subtitle(message):
+    if message.content_type == 'document':
+        file_id = message.document.file_id
+        file_info = bot.get_file(file_id)
+        file_path = file_info.file_path
+        subtitle_extension = message.document.file_name.split('.')[-1].lower()
+        if subtitle_extension not in ['srt', 'ass']:
+            bot.send_message(message.chat.id, "Invalid subtitle file format. Please send a .srt or .ass file.")
+        else:
+            bot.send_message(message.chat.id, "Processing... This may take some time.")
+            start_time = time.time()
+            video_path = download_video(file_path)
+            subtitle_path = download_subtitle(file_path)
+            video_with_subtitles = burn_subtitles(video_path, subtitle_path)
+            end_time = time.time()
+            processing_time = end_time - start_time
+            bot.send_message(message.chat.id, f"Processing time: {processing_time:.2f} seconds")
+            bot.send_video(message.chat.id, open(video_with_subtitles, 'rb'))
 
 def download_video(file_path):
     # Download the video from Telegram and return the local path
@@ -31,8 +42,13 @@ def download_video(file_path):
     # Store it locally with a unique name
     pass
 
+def download_subtitle(file_path):
+    # Download the subtitle from Telegram and return the local path
+    # You can use the 'requests' library or other methods to download the file
+    # Store it locally with a unique name
+    pass
+
 def burn_subtitles(video_path, subtitle_path):
-    # Use moviepy to burn subtitles into the video
     video = VideoFileClip(video_path)
     subtitles = SubtitlesClip(subtitle_path)
     final_video = video.set_subtitles(subtitles)
