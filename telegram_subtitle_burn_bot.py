deaf8be88b57d3f5fdf38189ab09dfca6554ed60
est-anime <134935100+est-anime@users.noreply.github.com>
2023-10-11 22:11:53 +0530

    Update telegram_subtitle_burn_bot.py

--- telegram_subtitle_burn_bot.py
+++ telegram_subtitle_burn_bot.py
@@ -13,15 +13,15 @@ bot = telebot.TeleBot(TOKEN)
 def handle_start(message):
     bot.send_message(message.chat.id, "Welcome to Video Encoder! Send a video to burn subtitles.")
 
-def download_file(file_path):
+def download_file(file_path, chat_id):
     file_info = bot.get_file(file_path)
     if not file_info:
-        bot.send_message(message.chat.id, "Error: File not found or unavailable.")
+        bot.send_message(chat_id, "Error: File not found or unavailable.")
         return None
 
     response = requests.get('https://api.telegram.org/file/bot{0}/{1}'.format(TOKEN, file_path))
     if response.status_code != 200:
-        bot.send_message(message.chat.id, "Error: Failed to download the file.")
+        bot.send_message(chat_id, "Error: Failed to download the file.")
         return None
 
     with NamedTemporaryFile(delete=False) as temp_file:
@@ -30,27 +30,28 @@ def download_file(file_path):
 
 @bot.message_handler(content_types=['video'])
 def handle_video(message):
-    bot.send_message(message.chat.id, "Received a video. Now please send the subtitle file (in .srt or .ass format) for burning into the video.")
+    chat_id = message.chat.id
+    bot.send_message(chat_id, "Received a video. Now please send the subtitle file (in .srt or .ass format) for burning into the video.")
     # Store the received video message and user chat ID for later use
-    bot.register_next_step_handler(message, process_subtitle)
+    bot.register_next_step_handler(message, process_subtitle, chat_id)
 
-def process_subtitle(message):
+def process_subtitle(message, chat_id):
     if message.content_type == 'document':
         file_id = message.document.file_id
-        file_path = download_file(file_id)
+        file_path = download_file(file_id, chat_id)  # Pass the chat_id to download_file
         subtitle_extension = message.document.file_name.split('.')[-1].lower()
         if subtitle_extension not in ['srt', 'ass']:
-            bot.send_message(message.chat.id, "Invalid subtitle file format. Please send a .srt or .ass file.")
+            bot.send_message(chat_id, "Invalid subtitle file format. Please send a .srt or .ass file.")
         else:
-            bot.send_message(message.chat.id, "Processing... This may take some time.")
+            bot.send_message(chat_id, "Processing... This may take some time.")
             start_time = time.time()
-            video_path = download_file(file_path)
-            subtitle_path = download_file(file_id)
+            video_path = download_file(file_id, chat_id)  # Pass the chat_id to download_file
+            subtitle_path = download_file(file_id, chat_id)  # Pass the chat_id to download_file
             video_with_subtitles = burn_subtitles(video_path, subtitle_path)
             end_time = time.time()
             processing_time = end_time - start_time
-            bot.send_message(message.chat.id, f"Processing time: {processing_time:.2f} seconds")
-            bot.send_video(message.chat.id, open(video_with_subtitles, 'rb'))
+            bot.send_message(chat_id, f"Processing time: {processing_time:.2f} seconds")
+            bot.send_video(chat_id, open(video_with_subtitles, 'rb'))
 
 def burn_subtitles(video_path, subtitle_path):
     video = VideoFileClip(video_path)
